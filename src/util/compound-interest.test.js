a2c6b65ea4adf1bef029a34b73f3a3e1d3fe1786
Jmeas <jellyes2@gmail.com>
2017-11-18 16:40:11 -0800

    Add compound interest calculator; update index file

---
+++ src/util/compound-interest.test.js
@@ -0,0 +1,57 @@
+import compoundInterest from './compound-interest';
+
+describe('compoundInterest', () => {
+  it('computes no inputs as 0', () => {
+    const result = compoundInterest();
+    expect(result).toEqual(0);
+  });
+
+  it('computes one year correctly; no contributions; contributions made at end', () => {
+    const result = compoundInterest({
+      principal: 1000,
+      annualContribution: 0,
+      numberOfYears: 1,
+      interestRate: 0.07,
+      contributionsMadeAtStart: false
+    });
+
+    expect(result).toEqual(1070);
+  });
+
+  it('computes 10 years correctly with no interest', () => {
+    const result = compoundInterest({
+      principal: 1000,
+      annualContribution: 100,
+      numberOfYears: 10,
+      interestRate: 0.0,
+      contributionsMadeAtStart: false
+    });
+
+    expect(result).toEqual(2000);
+  });
+
+  it('computes 5 years correctly with 7% interest; no contribution; contributions made at end', () => {
+    const result = compoundInterest({
+      principal: 1000,
+      annualContribution: 0,
+      numberOfYears: 5,
+      interestRate: 0.07,
+      contributionsMadeAtStart: false
+    });
+
+    expect(result).toBeCloseTo(1402.5517307);
+  });
+
+  it('computes 10 years correctly with 10% interest; substantial contribution; contributions made at start', () => {
+    const result = compoundInterest({
+      principal: 365000,
+      annualContribution: 365000,
+      numberOfYears: 10,
+      interestRate: 0.1,
+      contributionsMadeAtStart: true
+    });
+
+    expect(result).toBeCloseTo(7345591.975238);
+  });
+});
+
