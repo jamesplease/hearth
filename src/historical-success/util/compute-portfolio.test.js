a2d3bcb3edb372a20dd96c28ec43cfe811145192
Jmeas <jellyes2@gmail.com>
2017-12-10 12:43:34 -0800

    Add computePortfolio util

---
+++ src/historical-success/util/compute-portfolio.test.js
@@ -0,0 +1,115 @@
+import { fromTotalAndPercentages, fromInvestments } from './compute-portfolio';
+
+describe('computePortfolio', () => {
+  describe('fromInvestments', () => {
+    it('computes a single-investment portfolio correctly', () => {
+      const result = fromInvestments({
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000
+          }
+        ]
+      });
+
+      expect(result).toEqual({
+        totalValue: 10000,
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000,
+            percentage: 1
+          }
+        ]
+      });
+    });
+
+    it('computes a two-investment portfolio correctly', () => {
+      const result = fromInvestments({
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000
+          },
+          {
+            type: 'bonds',
+            value: 10000
+          }
+        ]
+      });
+
+      expect(result).toEqual({
+        totalValue: 20000,
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000,
+            percentage: 0.5
+          },
+          {
+            type: 'bonds',
+            value: 10000,
+            percentage: 0.5
+          }
+        ]
+      });
+    });
+  });
+
+  describe('fromTotalAndPercentages', () => {
+    it('computes a single-investment portfolio correctly', () => {
+      const result = fromTotalAndPercentages({
+        totalValue: 10000,
+        investments: [
+          {
+            type: 'stocks',
+            percentage: 1
+          }
+        ]
+      });
+
+      expect(result).toEqual({
+        totalValue: 10000,
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000,
+            percentage: 1
+          }
+        ]
+      });
+    });
+
+    it('computes a two-investment portfolio correctly', () => {
+      const result = fromTotalAndPercentages({
+        totalValue: 20000,
+        investments: [
+          {
+            type: 'stocks',
+            percentage: 0.5
+          },
+          {
+            type: 'bonds',
+            percentage: 0.5
+          }
+        ]
+      });
+
+      expect(result).toEqual({
+        totalValue: 20000,
+        investments: [
+          {
+            type: 'stocks',
+            value: 10000,
+            percentage: 0.5
+          },
+          {
+            type: 'bonds',
+            value: 10000,
+            percentage: 0.5
+          }
+        ]
+      });
+    });
+  });
+});
