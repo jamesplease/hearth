4fd3e850bdef0f6d18712777a7e92f4a990f9200
Jmeas <jellyes2@gmail.com>
2017-12-14 09:33:30 -0800

    Update About text

--- src/meta/about.js
+++ src/meta/about.js
@@ -6,18 +6,28 @@ export default class About extends Component {
       <div className="standardPage">
         <h1 className="primaryHeader">About</h1>
         <p className="appParagraph">
-          Being financially independent or early retired comes with its own set
-          of challenges. It can be difficult to determine where and when to
-          withdraw your funds from, or how to minimize your tax burden each
-          year.
+          Reaching early retirement sounds like a dream, but it's not without
+          its own unique set of challenges. Ensuring that your investments last
+          as long as you do can involve some tricky math and knowledge of the US
+          Tax Code.
         </p>
         <p className="appParagraph">
-          If you're not yet FIRE'd, you may have a lot of questions. There is a
-          lot of jargon to learn to prepare and plan to one day be FIRE'd.
+          For those of us who aren't yet FIRE'd, you may have a lot of
+          questions. What even is this FIRE thing, anyway? Perhaps you know the
+          answer to that question, and are already working toward your own
+          financial independence. Either way, learning the ropes takes time, and
+          resources are scattered all throughout the internet.
         </p>
         <p className="appParagraph">
-          No matter where you are in your journey to FIRE, Hearth is intended to
-          provide tools to help you achieve your goals.
+          To make matters worse, searching the web for resources related to
+          investing and money often leads to misleading, or otherwise bad
+          advice.
+        </p>
+        <p className="appParagraph">
+          Hearth is intended to provide a easy-to-understand information on
+          financial independence, as well as tools to help you plan and execute
+          your early retirement. No matter where you are in your path to FIRE,
+          we hope that you find Hearth useful.
         </p>
       </div>
     );
