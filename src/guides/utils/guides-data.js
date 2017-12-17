62a2a4fb9cc23c891071f76cb792e8f41e36c28c
Jmeas <jellyes2@gmail.com>
2017-12-16 23:58:13 -0800

    Update styles; remove placeholder content

--- src/guides/utils/guides-data.js
+++ src/guides/utils/guides-data.js
@@ -9,16 +9,5 @@ export default [
     url: '/glossary',
     description:
       'One of the hardest parts of FIRE is the number of terms and concepts to learn. The glossary is a quick reference to many of them.'
-  },
-  {
-    name: 'Mega Backdoor Roth',
-    url: '/mega-backdoor-roth',
-    description:
-      'The Mega Backdoor Roth is a system that allows you to contribute a lot of money to a Roth IRA.'
-  },
-  {
-    name: 'Roth IRA Conversion Pipeline',
-    url: '/roth-ira-conversion-pipeline',
-    description: 'Access your tax-advantaged funds earlier using this strategy.'
   }
 ];
