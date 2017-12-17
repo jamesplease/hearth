63c74cea58ed7296decef0e00554c2a3546449ed
Jmeas <jellyes2@gmail.com>
2017-12-17 00:01:24 -0800

    Remove "all rights reserved" wording

--- src/common/footer.js
+++ src/common/footer.js
@@ -5,9 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="footerContent">
-          Copyright © Hearth 2017. All rights reserved.
-        </div>
+        <div className="footerContent">Copyright © Hearth 2017.</div>
       </footer>
     );
   }
