8ab4081a851e51884b045f68d65662314d259541
Jmeas <jellyes2@gmail.com>
2018-01-06 22:50:15 -0800

    Add CSP + gzip compression to server

--- server/index.js
+++ server/index.js
@@ -14,14 +14,43 @@
 
 require('./utils/uncaught-things');
 
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
 const log = require('./utils/log');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 app.set('port', port);
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: [
+          "'self'",
+          // Normalize
+          'cdnjs.cloudflare.com',
+          // Material Design Icon font
+          'cdn.materialdesignicons.com',
+          // Google fonts
+          'fonts.googleapis.com'
+        ],
+        fontSrc: [
+          "'self'",
+          // Material Design Icon font
+          'cdn.materialdesignicons.com',
+          // Google fonts
+          'fonts.gstatic.com'
+        ]
+      }
+    }
+  })
+);
+app.use(compression());
 
 // Serve our static files first
 const staticDir = path.join(__dirname, '..', 'build');
